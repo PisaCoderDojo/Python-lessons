2d3c6735ff5cf87c39174e76a7f01cd9eadf9214
lucarin91 <lucarin91@gmail.com>
2015-05-13 21:02:54 +0200

    bug

---
+++ 3-bug-SemanticError.py
@@ -0,0 +1,15 @@
+#Trova l'errore in queto programma
+
+import random
+
+number1 = random.randint(1, 10)
+number2 = random.randint(1, 10)
+
+print('Quant\'è ' + str(number1) + ' + ' + str(number2) + '?')
+
+answer = input()
+
+if answer == number1 + number2:
+    print('Corretto!')
+else:
+    print('No! La risposta giusta è ' + str(number1 + number2))
---
+++ 3-bug-SyntaxErrors.py
@@ -0,0 +1,14 @@
+#Trova l'errore in questo programma
+
+import random
+print('testa o croce?\n0 -> testa\n1 -> croce')
+risp = input()
+
+# 0 -> testa
+# 1 -> croce
+moneta = str(random.randint(0,1))
+
+if (risp == moneta):
+    print ('Hai vinto! è uscito ' + moneta)
+else
+    print ('Hai perso! è uscito ' + moneta)
